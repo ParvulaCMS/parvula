076b9c4874243d092169c23b75ed7ab08214559f
Fabien Sa <fabacrans@hotmail.com>
2013-10-21 22:01:22 +0200

    IOException extends RuntimeException

--- parvula/core/exception/IOException.php
+++ parvula/core/exception/IOException.php
@@ -4,10 +4,10 @@ namespace Parvula\Core\Exception;
 
 /**
  * IOException
- * 
+ *
  * @package Parvula
  * @version 0.1.0
  * @author Fabien Sa
  * @license MIT License
  */
-class IOException extends \Exception { }
+class IOException extends \RuntimeException { }
