2c8fdfd1223f9da5a1e8bd8dfe5269a53cabae0a
Fabien Sa <fabacrans@hotmail.com>
2013-11-16 00:21:01 +0100

    new MarkdownPageSerializer

---
+++ parvula/core/MarkdownPageSerializer.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Parvula\Core;
+
+/**
+ * MarkdownPageSerializer class
+ *
+ * @package Parvula
+ * @version 0.1.0
+ * @since 0.2.0
+ * @author Fabien Sa
+ * @license MIT License
+ */
+class MarkdownPageSerializer implements PageSerializerInterface {
+
+	/**
+	 * Serialize page
+	 * @param Page $page
+	 * @return boolean
+	 */
+	public function serialize(Page $page) {
+
+		$header = PHP_EOL;
+		$header .= 'title: ' . $page->title;
+		$header .= 'description' . $page->$description;
+		$header .= 'author' . $page->$author;
+		$header .= 'date' . $page->$date;
+		$header .= 'robots' . $page->$robots;
+
+		$header .= PHP_EOL . PHP_EOL . str_repeat('-', 5) . PHP_EOL . PHP_EOL;
+
+		$content = $page->content;
+
+		return $header . $content;
+	}
+
+	/**
+	 * Unserialize data
+	 * @param string $data
+	 * @return Page
+	 */
+	public function unserialize($filePath, $data = null) {
+		if($data === null) {
+			$data = $filePath;
+			$filePath = '';
+		}
+
+		$infos = preg_split("/\s[-=]{3,}\s/", $data, 2);
+
+		$headerData = trim($infos[0]);
+		preg_match_all("/(\w+)[\s:=]+(.+)/", $headerData, $headerMatches);
+
+		$page = new Page();
+		// Set page headers fields
+		$page->url = $filePath;
+		for ($i = 0; $i < count($headerMatches[1]); ++$i) {
+			$key = trim($headerMatches[1][$i]);
+			$page->{$key} = rtrim($headerMatches[2][$i], "\r\n");
+		}
+
+		$page->content = $infos[1];
+
+		return $page;
+	}
+
+}
--- parvula/core/ParvulaPageSerializer.php
+++ parvula/core/ParvulaPageSerializer.php
@@ -2,16 +2,18 @@
 
 namespace Parvula\Core;
 
+use Parvula\Core\MarkdownPageSerializer;
+
 /**
  * ParvulaPageSerializer class
  *
  * @package Parvula
- * @version 0.1.0
+ * @version 0.2.0
  * @since 0.1.0
  * @author Fabien Sa
  * @license MIT License
  */
-class ParvulaPageSerializer implements PageSerializerInterface {
+class ParvulaPageSerializer extends MarkdownPageSerializer implements PageSerializerInterface {
 
 	/**
 	 * Serialize page
@@ -19,19 +21,7 @@ class ParvulaPageSerializer implements PageSerializerInterface {
 	 * @return boolean
 	 */
 	public function serialize(Page $page) {
-
-		$header = PHP_EOL;
-		$header .= 'title: ' . $page->title;
-		$header .= 'description' . $page->$description;
-		$header .= 'author' . $page->$author;
-		$header .= 'date' . $page->$date;
-		$header .= 'robots' . $page->$robots;
-
-		$header .= PHP_EOL . PHP_EOL . str_repeat('-', 5) . PHP_EOL . PHP_EOL;
-
-		$content = $page->content;
-
-		return $header . $content;
+		return false;
 	}
 
 	/**
@@ -40,29 +30,11 @@ class ParvulaPageSerializer implements PageSerializerInterface {
 	 * @return Page
 	 */
 	public function unserialize($filePath, $data = null) {
-		if($data === null) {
-			$data = $filePath;
-			$filePath = '';
-		}
 
-		$infos = preg_split("/\s[-=]{3,}\s/", $data, 2);
-
-		$headerData = trim($infos[0]);
-		preg_match_all("/(\w+)[\s:=]+(.+)/", $headerData, $headerMatches);
-
-		$page = new Page();
-		// Set page headers fields
-		$page->url = $filePath;
-		for ($i = 0; $i < count($headerMatches[1]); ++$i) {
-			$key = trim($headerMatches[1][$i]);
-			$page->{$key} = rtrim($headerMatches[2][$i], "\r\n");
-		}
-
-		$content = $infos[1];
-
+		$page = parent::unserialize($filePath, $data);
 		$parser = new MarkdownParvula;
 
-		$page->content = $parser->transform($content);
+		$page->content = $parser->transform($page->content);
 
 		return $page;
 	}
