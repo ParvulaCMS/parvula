1012f0f2d7a8e3a5441e88236385aabc3da3bb5e
Fabien Sa <fabacrans@hotmail.com>
2013-11-24 00:46:55 +0100

    add constants
    add ADMIN and VERSION

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-// --------------- \\
-//  P A R V U L A  \\
-//      C M S      \\
-// --------------- \\
+//| --------------- |\\
+//|  P A R V U L A  |\\
+//|      C M S      |\\
+//| --------------- |\\
 
 define('ROOT', '');
 define('APP', ROOT . 'parvula/');
-define('CORE', ROOT . 'core/');
 define('VENDOR', ROOT . 'vendor/');
 define('DATA', ROOT . 'data/');
 define('PAGES', DATA . 'pages/');
 define('TMPL', ROOT . 'templates/');
+define('ADMIN', ROOT . 'admin/');
+define('VERSION', '0.3.0');
 
 require APP . 'bootstrap.php';
