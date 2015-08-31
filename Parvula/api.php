cbdc0d4d7d0a51d8c7c9e0c56859181d1302a37a
BafS <fabacrans@hotmail.com>
2015-08-31 02:13:16 +0200

    Update a page (test), variable apiSerializer

--- Parvula/api.php
+++ Parvula/api.php
@@ -9,10 +9,11 @@ use Parvula\Core\Parvula;
 
 if(!defined('ROOT')) exit;
 
-$apiDefaultPageSerializer = Config::apiDefaultPageSerializer();
+$defaultPageSerializer = Config::defaultPageSerializer();
 
-$parvula = new Parvula(new $apiDefaultPageSerializer);
+$parvula = new Parvula(new $defaultPageSerializer);
 
+$apiSerializer = 'json_encode'; //@TODO
 
 //
 // Public API
@@ -25,9 +26,8 @@ $router->get('/pages/::name', function($req) use ($parvula) {
 
 // Array<Page> of Pages
 $router->get('/pages', function($req) use ($parvula) {
-	echo json_encode($parvula->getPages());
+	echo $apiSerializer($parvula->getPages());
 });
-
 
 //
 // Admin API
@@ -36,28 +36,46 @@ if(true === isParvulaAdmin()) {
 
 	// List of pages. Array<string> of pages paths
 	$router->get('/pageslist', function($req) use ($parvula) {
-		echo json_encode($parvula->listPages());
+		echo $apiSerializer($parvula->listPages());
 	});
 
 	// Delete page
-	$router->delete('/pages/:name', function($req) use ($parvula) {
-		echo json_encode($parvula->deletePage($req->params->name));
+	$router->delete('/pages/::name', function($req) use ($parvula) {
+		echo $apiSerializer($parvula->deletePage($req->params->name));
 	});
 
 	// Save page
-	$router->put('/pages/:name', function($req) use ($parvula, $apiDefaultPageSerializer) {
-		if(!isset($req->params->name) ||  trim($req->params->name) === '') {
+	$router->put('/pages/::name', function($req) use ($parvula, $defaultPageSerializer) {
+		if(!isset($req->params->name) || trim($req->params->name) === '') {
 			return false;
 		}
 
 		$page = Page::pageFactory($req->body);
-		echo json_encode($parvula->setPage($page, $req->params->name, new $apiDefaultPageSerializer));
+		echo $apiSerializer($parvula->setPage($page, $req->params->name, new $defaultPageSerializer));
+	});
+
+	// Update page @TODO TEST
+	$router->post('/pages/::name', function($req) use ($parvula, $defaultPageSerializer) {
+		if(!isset($req->params->name) || trim($req->params->name) === '') {
+			return false;
+		}
+
+		// Get old page and update new fields
+		$page = $parvula->getPage($req->params->name);
+		foreach ($req->body as $key => $value) {
+			$page->{$key} = $value;
+		}
+
+		echo $apiSerializer($parvula->updatePage($page, $req->params->name, new $defaultPageSerializer));
 	});
 
 	// Logout
 	$router->any('/logout', function() {
 		session_destroy();
-		echo json_encode(session_unset());
+		echo $apiSerializer(session_unset());
 	});
 
+} else {
+	// @TODO
+	// echo '{"message": "Not found or not logged"}';
 }
