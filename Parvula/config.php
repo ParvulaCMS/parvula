8cd0ed3f8b58f7ab098a3bf083857d0d0d341aa5
BafS <fabacrans@hotmail.com>
2015-08-31 02:06:14 +0200

    update the config

--- Parvula/config.php
+++ Parvula/config.php
@@ -15,9 +15,9 @@ return [
 		'Asset' => 'Parvula\Core\Asset'
 	],
 
-	// Plugins to load
-	'plugins' => [
-		// 'Plugin\Gallery\Gallery'
+	// List of disabled plugins @TODO
+	'disabledPlugins' => [
+
 	],
 
 	// You can force this option with a boolean or leave it on 'auto' detection
@@ -29,7 +29,7 @@ return [
 	// Error page
 	'errorPage' => '_404',
 
-	// Extension for files in ./data
+	// Extension for files in ./data/pages
 	'fileExtension' => 'md',
 
 	// How to sort pages (php.net/manual/en/function.array-multisort.php)
@@ -39,13 +39,15 @@ return [
 	'sortField' => 'index',
 
 	// Config file to read
-	'userConfig' => 'site.conf',
+	'userConfig' => 'site.conf.php',
+
+	// Class to parse pages (must implements ContentParserInterface), can be null
+	// 'defaultContentParser' => null,
+	'defaultContentParser' => 'Parvula\Core\Parser\MarkdownContentParser',
 
 	// Class to (un)serialize pages (must implements PageSerializerInterface)
-	'defaultPageSerializer' => 'Parvula\Core\MarkdownPageSerializer',
-
-	// API only - Class to (un)serialize pages (must implements PageSerializerInterface)
-	'apiDefaultPageSerializer' => 'Parvula\Core\ParvulaPageSerializer',
+	'defaultPageSerializer' => 'Parvula\Core\Serializer\ParvulaPageSerializer',
+	// 'defaultPageSerializer' => 'Parvula\Core\Serializer\ParvulaJsonPageSerializer',
 
 	// Administration URL (/parvula-admin by default)
 	'adminURL' => 'parvula-admin',
