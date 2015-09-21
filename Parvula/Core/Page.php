5cf324bd9c18b97a8cda36e6d27d3cd96479f937
BafS <fabacrans@hotmail.com>
2015-09-21 20:47:26 +0200

    remove `id` field, compare with `slug`, add exception if slug not ok and update doc

--- Parvula/Core/Page.php
+++ Parvula/Core/Page.php
@@ -30,43 +30,54 @@ class Page {
 	 */
 	public $content;
 
-	/**
-	 * @var string
-	 */
-	private $id;
 	// public $hidden;
 	// public $index;
 
 	/**
 	 * Page factory, create a new page from an array
+	 * The parameter $pageInfo must contain, at least, the `title` and `slug` fields.
+	 * The `slug` need to be normalized (a-z0-9@-_+./).
+	 *
 	 * @param array $pageInfo Array with page information (must contain `title` and `slug` fields)
-	 * @throws PageException if $pageInfo does not have field `title` and `slug`
+	 * @throws PageException if `$pageInfo` does not have field `title` and `slug`
+	 * @throws PageException if `$pageInfo[slug]` value is not normalized
 	 * @return Page The created Page
 	 */
 	public static function pageFactory(array $pageInfo) {
 		$page = new static;
 
+		// Check if $pageInfo array is complete
 		if (empty($pageInfo['title']) || empty($pageInfo['slug'])) {
 			throw new PageException('Page cannot be created, $pageInfo MUST contain `title` and `slug` fields');
 		} else if (!isset($pageInfo['content'])) {
 			$pageInfo['content'] = '';
 		}
 
+		if (!preg_match('/^[a-z0-9@-_\+\.\/]+$/', $pageInfo['slug'])) {
+			throw new PageException('Page cannot be created, $pageInfo[slug] (' .
+				htmlspecialchars($pageInfo['slug']) . ') value is not normalized');
+		}
+
 		foreach ($pageInfo as $field => $value) {
 			$page->$field = $value;
 		}
 
-		$page->id = hash('crc32b', trim($page->slug, ' /'));
-
 		return $page;
 	}
 
+	/**
+	 * Compare this page with an other (compare the slug)
+	 *
+	 * @param Page $page2
+	 * @return boolean True if both pages are the same
+	 */
 	public function is(Page $page2) {
-		return $this->id === $page2->id;
+		return $this->slug === $page2->slug;
 	}
 
 	/**
 	 * Override `tostring` when print this object
+	 *
 	 * @return string
 	 */
 	public function __tostring() {
