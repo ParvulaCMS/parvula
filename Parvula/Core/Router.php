d21f49cb3a9686d5611b95f63c6bac95572d7760
BafS <fabacrans@hotmail.com>
2015-10-06 12:08:09 +0200

    rename space to group, $callback must be callable, use uri instead of path

--- Parvula/Core/Router.php
+++ Parvula/Core/Router.php
@@ -40,7 +40,6 @@ class Router {
 	 */
 	public function __construct() {
 		$this->routes = new \SplDoublyLinkedList;
-
 		$this->prefix = '';
 	}
 
@@ -66,6 +65,7 @@ class Router {
 	 * @param function $callback
 	 * @return Router Self
 	 */
+	public function get($path, callable $callback) {
 		return $this->on('GET', $path, $callback);
 	}
 
@@ -75,6 +75,7 @@ class Router {
 	 * @param function $callback
 	 * @return Router Self
 	 */
+	public function post($path, callable $callback) {
 		return $this->on('POST', $path, $callback);
 	}
 
@@ -84,6 +85,7 @@ class Router {
 	 * @param function $callback
 	 * @return Router Self
 	 */
+	public function put($path, callable $callback) {
 		return $this->on('PUT', $path, $callback);
 	}
 
@@ -93,6 +95,7 @@ class Router {
 	 * @param function $callback
 	 * @return Router Self
 	 */
+	public function delete($path, callable $callback) {
 		return $this->on('DELETE', $path, $callback);
 	}
 
@@ -103,6 +106,7 @@ class Router {
 	 * @param function $callback
 	 * @return Router Self
 	 */
+	public function any($path, callable $callback) {
 		return $this->on('*', $path, $callback);
 	}
 
@@ -113,6 +117,7 @@ class Router {
 	 * @param function $callback
 	 * @return Router Self
 	 */
+	public function on($method, $path, callable $callback) {
 		$this->routes->push([
 			"method" => $method,
 			"path" => $this->prefix . $path,
@@ -123,10 +128,12 @@ class Router {
 	}
 
 	/**
+	 * Group, to prefix a group of routes
 	 * @param string $prefix
 	 * @param function $callback
 	 * @return Router Self
 	 */
+	public function group($prefix, callable $callback) {
 		$that = clone $this;
 		$that->prefix = $prefix;
 		$callback($that);
@@ -138,6 +145,10 @@ class Router {
 	 * Run the router
 	 * @return mixed
 	 */
+	public function run($method, $uri) {
+		$this->method = $method;
+		$this->uri = $uri;
+
 		$this->routes->rewind();
 		return $this->dispatch();
 	}
@@ -157,10 +168,12 @@ class Router {
 				$regex = $this->normalizeRegex($route['path']);
 
 				// if path is OK
+				if(preg_match("@^{$regex}@", $this->uri, $matches)) {
 					$callback = $route['callback'];
 
 					$req = new \ArrayObject;
 					$req->params = (object)$matches;
+					$req->uri = $this->uri;
 					$req->uri = $this->uri;
 					$req->query = $this->query;
 
@@ -174,6 +187,7 @@ class Router {
 					$that = $this;
 					$next = function() use ($that) {
 						$that->routes->next();
+						return $that->dispatch();
 					};
 
 					return $callback($req, $next);
