a27206a23c102cafab373cb21bb654ebb0ba9c8e
BafS <fabacrans@hotmail.com>
2015-10-10 11:06:40 +0200

    add ArrayAccess methods

--- Parvula/Core/Container.php
+++ Parvula/Core/Container.php
@@ -2,10 +2,21 @@
 
 namespace Parvula\Core;
 
+use Closure;
+use ArrayAccess;
 use Parvula\Core\ContainerInterface;
 use Parvula\Core\Exception\NotFoundException;
 
-class Container
+/**
+ * Container
+ *
+ * @package Parvula
+ * @version 0.5.0
+ * @since 0.5.0
+ * @author Fabien Sa
+ * @license MIT License
+ */
+class Container implements ArrayAccess
 {
 	/**
 	 * @var array Instances
@@ -47,7 +58,7 @@ class Container
 	 * @param string $id Identifier of the entry to look for
 	 * @param Closure $closure
 	 */
-	public function add($id, \Closure $closure) {
+	public function add($id, Closure $closure) {
 		$this->instances[$id] = $closure;
 	}
 
@@ -57,9 +68,24 @@ class Container
 	 * @param string $id Identifier of the entry to look for
 	 * @param Closure $closure
 	 */
-	public function share($id, \Closure $closure) {
+	public function share($id, Closure $closure) {
 		$this->sharedInstances[$id] = true;
 		$this->instances[$id] = $closure;
+	}
+
+	/**
+	 * Remove entry from the container
+	 *
+	 * @param string $id Identifier of the entry to look for
+	 */
+	public function remove($id) {
+		if($this->has($id)) {
+			if(isset($this->sharedInstances)) {
+				unset($this->sharedInstances);
+			}
+
+			unset($this->instance[$id]);
+		}
 	}
 
 	/**
@@ -71,5 +97,41 @@ class Container
 	 */
 	public function has($id) {
 		return isset($this->instances[$id]) && $this->instances[$id] !== null;
+	}
+
+	/**
+	 * Alias for `get`
+	 *
+	 * @see Container::get
+	 */
+	public function offsetGet($id) {
+		return $this->get($id);
+	}
+
+	/**
+	 * Alias for `add`
+	 *
+	 * @see Container::add
+	 */
+	public function offsetSet($id, $closure) {
+		$this->add($id, $closure);
+	}
+
+	/**
+	 * Alias for `has`
+	 *
+	 * @see Container::has
+	 */
+	public function offsetExists($id) {
+		return $this->has($id);
+	}
+
+	/**
+	 * Alias for `remove`
+	 *
+	 * @see Container::remove
+	 */
+	public function offsetUnset($id) {
+		$this->remove($id);
 	}
 }
