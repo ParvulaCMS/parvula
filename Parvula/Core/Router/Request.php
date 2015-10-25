c0fdd40e6e6d2cc01bfc501b91dbd2bb6015c05f
BafS <fabacrans@hotmail.com>
2015-10-25 02:51:47 +0100

    add `uri`, `scheme` and `secureLayer` fields

--- Parvula/Core/Router/Request.php
+++ Parvula/Core/Router/Request.php
@@ -22,7 +22,22 @@ class Request
 
 	public $query;
 
-	public  $params;
+	public $params;
+
+	/**
+	 * @var string
+	 */
+	public $uri;
+
+	/**
+	 * @var string Scheme (without the `://`)
+	 */
+	public $scheme;
+
+	/**
+	 * @var bool If server use a cryptographic protocol (like TLS/SSL)
+	 */
+	public $secureLayer;
 
 
 	/**
@@ -47,6 +62,15 @@ class Request
 		// Query
 		isset($server['QUERY_STRING']) ? parse_str($server['QUERY_STRING'], $this->query) : '';
 		$this->query = (object) $this->query;
+
+		$this->host = isset($server['HTTP_HOST']) ? $server['HTTP_HOST'] : '';
+
+		$this->uri = isset($server['REQUEST_URI']) ? $server['REQUEST_URI'] : '';
+
+		// http, https, ...
+		$this->scheme = isset($server['REQUEST_SCHEME']) ? $server['REQUEST_SCHEME'] : '';
+
+		$this->secureLayer = isset($server['HTTPS']) ? $server['HTTPS'] === 'on' : false;
 
 		// Body
 		$this->body = (object) $post;
