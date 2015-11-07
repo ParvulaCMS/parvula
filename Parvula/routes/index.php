399ad9ec42061e9df75c577f907f64043edb6d25
BafS <fabacrans@hotmail.com>
2015-11-07 13:47:33 +0100

    Clean syntax, replace `self` by `page`

--- Parvula/routes/index.php
+++ Parvula/routes/index.php
@@ -13,7 +13,7 @@ $router->map('GET|POST', '/{slug:[a-z0-9\-_\+\/]*}', function($req) use($app) {
 	$plugins->trigger('uri', [$req->params->slug]);
 	$plugins->trigger('slug', [$slug]);
 
-	if($slug === '') {
+	if ($slug === '') {
 		$slug = $app['config']->get('homePage');
 	}
 
@@ -31,7 +31,7 @@ $router->map('GET|POST', '/{slug:[a-z0-9\-_\+\/]*}', function($req) use($app) {
 	$plugins->trigger('page', [&$page]);
 
 	// 404
-	if(false === $page) {
+	if (false === $page) {
 		// header(' ', true, 404);
 		header('HTTP/1.0 404 Not Found'); // Set header to 404
 		$page = $pages->read($app['config']->get('errorPage'));
@@ -49,19 +49,19 @@ $router->map('GET|POST', '/{slug:[a-z0-9\-_\+\/]*}', function($req) use($app) {
 
 		// Assign some useful variables
 		$view->addData([
-			'baseUrl' => Parvula::getRelativeURIToRoot(),
+			'baseUrl'  => Parvula::getRelativeURIToRoot(),
 			'themeUrl' => Parvula::getRelativeURIToRoot() . $theme->getPath() . '/',
-			'pages' =>
-				function($listHidden = false, $pagesPath = null) use($pages) {
+			'pages'    =>
+				function($listHidden = false, $pagesPath = null) use ($pages) {
 					return $pages->all($pagesPath)->visible()->order(SORT_ASC)->toArray();
 				},
-			'plugin' =>
-				function($name) use($plugins) {
+			'plugin'   =>
+				function($name) use ($plugins) {
 					return $plugins->getPlugin($name);
 				},
-			'site' => $app['config']->toObject(),
-			'self' => $page,
-			'__time__' => function() use($app) {
+			'site'     => $app['config']->toObject(),
+			'page'     => $page,
+			'__time__' => function () use ($app) {
 				// useful to benchmark
 				return sprintf('%.4f', $app['config']->get('__time__') + microtime(true));
 			}
@@ -73,18 +73,19 @@ $router->map('GET|POST', '/{slug:[a-z0-9\-_\+\/]*}', function($req) use($app) {
 			$layout = 'index';
 		}
 
-		$plugins->trigger('beforeRender', [&$layout]);
+		$plugins->trigger('preRender', [&$layout]);
 		$out = $view->render($layout);
 		$plugins->trigger('afterRender', [&$out]);
+		$plugins->trigger('postRender', [&$out]);
 		return $out;
 
-	} catch(Exception $e) {
+	} catch (Exception $e) {
 		exceptionHandler($e); // TODO
 	}
 });
 
 // Files handler (media or uploads) (must have an extension)
-$router->get('/{file:.+\.[^.]{2,8}}', function($req) use($app) {
+$router->get('/{file:.+\.[^.]{2,8}}', function ($req) use ($app) {
 
 	$filePath = str_replace('..', '', $req->params->file);
 	$ext  = pathinfo($filePath, PATHINFO_EXTENSION);
