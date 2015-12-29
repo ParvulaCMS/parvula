4a5019f315e4638db9c7e1d9d05048c8c4b3ebc7
BafS <fabacrans@hotmail.com>
2015-12-29 21:28:42 +0100

    Add documentation

--- Parvula/Core/Plugin.php
+++ Parvula/Core/Plugin.php
@@ -2,6 +2,8 @@
 
 namespace Parvula\Core;
 
+use DOMDocument;
+
 /**
  * Plugin class @TODO
  * Abstract class, need to be inherited to create a new plugin
@@ -19,8 +21,8 @@ namespace Parvula\Core;
  * @author Fabien Sa
  * @license MIT License
  */
-abstract class Plugin {
-
+abstract class Plugin
+{
 	/**
 	 * @var string Plugin path
 	 */
@@ -68,6 +70,13 @@ abstract class Plugin {
 		return Parvula::getRelativeURIToRoot() . $this->getPluginPath();
 	}
 
+	/**
+	 * Append string to the given element
+	 *
+	 * @param  string $html
+	 * @param  string $append
+	 * @return string Html ouput
+	 */
 	private function appendToElement($element, $html, $append) {
 		// @TODO a bit hacky, need to clean and find correctly the `</head>`
 
@@ -76,31 +85,43 @@ abstract class Plugin {
 		}
 
 		libxml_use_internal_errors(true); // html5 ok
-		$dom = new \DOMDocument();
+		$dom = new DOMDocument();
 		$dom->loadHTML($html);
 		$node = $dom->getElementsByTagName($element)->item(0);
 
+		$lineNo = 0;
 		if ($node) {
 			$lineNo = $node->lastChild->getLineNo();
-		} else {
-			$lineNo = 0;
 		}
 
 		$outArr = explode("\n", $html);
 
 		// Lines to skip to go to the end of the node
-		$n = substr_count($node->nodeValue, "\n");
-
-		$outArr[($lineNo - 1) + $n] .= PHP_EOL . $append;
+		$linesToSkip = substr_count($node->nodeValue, "\n");
+		$outArr[($lineNo - 1) + $linesToSkip] .= PHP_EOL . $append;
 
 		return implode($outArr, "\n");
 	}
 
+	/**
+	 * Append string to the header element (<head>)
+	 *
+	 * @param  string $html
+	 * @param  string $append
+	 * @return string Html ouput
+	 */
 	protected function appendToHeader($html, $append) {
 		return $this->appendToElement('head', $html, $append);
 	}
 
+	/**
+	 * Append string to the body element (<body>)
+	 *
+	 * @param  string $html
+	 * @param  string $append
+	 * @return string Html ouput
+	 */
 	protected function appendToBody($html, $append) {
-		return preg_replace("/(< ?\/body)/", $append . "$1", $html);
+		return preg_replace("/(< ?\/ ?body)/", $append . "$1", $html);
 	}
 }
