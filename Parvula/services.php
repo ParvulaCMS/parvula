bac928a7053747e45b62fa7462ae414253e8d3dc
BafS <fabacrans@hotmail.com>
2016-02-09 02:57:51 +0100

    Clean theme service, use updated index method

--- Parvula/services.php
+++ Parvula/services.php
@@ -123,7 +123,7 @@ $app->add('themes', function (Container $this) {
 	return new Parvula\Core\Model\Mapper\Themes(_THEMES_, $this['fileParser']);
 });
 
-$app->add('theme', function (Container $this) {
+$app->share('theme', function (Container $this) {
 	if ($this['themes']->has($themeName = $this['config']->get('theme'))) {
 		return $this['themes']->read($themeName);
 	} else {
@@ -135,34 +135,20 @@ $app->add('view', function (Container $this) {
 	$theme = $this['theme'];
 
 	// Create new Plates instance to render theme files
-	$view = new League\Plates\Engine($theme->getPath(), $theme->getExtension());
+	$path = $theme->getPath();
+	$view = new League\Plates\Engine($path, $theme->getExtension());
 
-	// Register theme folders
-	$iter = new RecursiveIteratorIterator(
-		new RecursiveDirectoryIterator($theme->getPath(), RecursiveDirectoryIterator::SKIP_DOTS),
-			RecursiveIteratorIterator::SELF_FIRST, RecursiveIteratorIterator::CATCH_GET_CHILD
-	);
-	$baseLen = strlen($theme->getPath());
-	foreach ($iter as $path => $dir) {
-		if ($dir->isDir()){
-			$name = str_replace([DIRECTORY_SEPARATOR, '/', '\\'], '|', substr($path, $baseLen));
+	// Register folder begining with a '_' as Plates folder
+	// (Plates will resolve `this->fetch('myFolder::file')` as `_myFolder/file.html`)
+	$filter = function ($current) {
+		// Must be a dir begining with _
+		return $current->isDir() && $current->getFilename()[0] === '_';
+	};
 
-			// Register '_*' folders
-			if ($name[0] === '_') {
-
-				if ($name === '_layouts') {
-					$isLayoutRegistered = true;
-				}
-
-				$view->addFolder(substr($name, 1), $path);
-			}
-		}
-	}
-
-	// Register 'layouts' with the layouts folder (in the theme config) if not registered
-	if (!$isLayoutRegistered) {
-		$view->addFolder('layouts', $theme->getPath() . $theme->getLayoutFolder());
-	}
+	$fs = new Files($path);
+	$flattened = $fs->index('', function ($file, $dir) use ($path, $view) {
+		$view->addFolder(substr($file, 1), $path . $dir . '/' . $file);
+	}, $filter);
 
 	return $view;
 });
