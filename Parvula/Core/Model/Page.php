e4761fef98be55f9fb753a7555790b47a0f4aa8a
BafS <fabacrans@hotmail.com>
2016-02-10 14:50:59 +0100

    Fix date is optional for creation

--- Parvula/Core/Model/Page.php
+++ Parvula/Core/Model/Page.php
@@ -77,7 +77,10 @@ class Page {
 			$this->{$key} = $value;
 		}
 
-		$this->date = new DateTime($this->date);
+		// date can be set by after the creation, by the database or file last edit
+		if (!empty($this->date)) {
+			$this->date = new DateTime($this->date);
+		}
 		$this->content = $content;
 		$this->sections = (object) $sections;
 	}
