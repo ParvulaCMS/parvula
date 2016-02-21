d1b5b69cc73860723580f21aa8cbc3d61bb6e3e6
BafS <fabacrans@hotmail.com>
2016-02-22 00:48:18 +0100

    Update Whoops 2 code and remove exceptionHandler

--- Parvula/helpers.php
+++ Parvula/helpers.php
@@ -27,23 +27,3 @@ function getPluginList(array $except = []) {
 
 	return $plugins;
 }
-
-/**
- * Handle exceptions
- *
- * @deprecated
- * @param Exception $e
- * @return
- */
-function exceptionHandler(Exception $e) {
-	$className = basename(str_replace('\\', '/', get_class($e)));
-
-	echo "<h2 style='font-size:24px'>Error</h2>\n",
-	"<pre style='background:#f8f8f8;padding:8px'>\n",
-	'<b>Caught ', $className,': "', $e->getMessage(),
-	'"</b> (', $e->getFile(), ':', $e->getLine(), ")\n\n",
-	'<span style="color:#811">', $e->getTraceAsString(), "</span>",
-	"\n</pre>";
-
-	exit;
-}
--- Parvula/services.php
+++ Parvula/services.php
@@ -8,16 +8,21 @@ use Parvula\Core\FilesSystem as Files;
 
 $app['errorHandler'] = function () {
 	if (class_exists('\\Whoops\\Run')) {
-		$whoops = new Whoops\Run();
-		$whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
-		// $jsonHandler = new Whoops\Handler\JsonResponseHandler();
-		// $jsonHandler->onlyForAjaxRequests(true);
-		// $whoops->pushHandler($jsonHandler);
+		$run = new Whoops\Run;
+		$handler = new Whoops\Handler\PrettyPageHandler;
 
-		$whoops->register();
-	} else {
-		// Use custom exception handler
-		set_exception_handler('exceptionHandler');
+		$handler->setPageTitle("Parvula Error");
+		$handler->addDataTable('Parvula', [
+		 	'Version'=> _VERSION_
+		]);
+
+		$run->pushHandler($handler);
+
+		if (Whoops\Util\Misc::isAjaxRequest()) {
+			$run->pushHandler(new Whoops\Handler\JsonResponseHandler);
+		}
+
+		$run->register();
 	}
 };
 
