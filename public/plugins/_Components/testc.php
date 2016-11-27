aff6c83ec39f69bb3dabdcc19818c507e5dd21e5
BafS <fabacrans@gmail.com>
2016-11-27 01:30:50 +0100

    Change structure [wip] (BC)
    Add `public` folder as the only accessible point from outside and move
    `static` to `storage`. Update constants and other paths info.

---
+++ public/plugins/_Components/testc.php
@@ -0,0 +1,12 @@
+<?= $page->title ?>
+<script src="https://gist.github.com/<?= str_replace([':', ' '], '/', trim($section->slug)) ?>.js"></script>
+<?php
+return [
+	'name' => 'Gist',
+	'props' => [
+		'slug' => [
+			'type' => 'string',
+			'required' => true
+		]
+	]
+];
