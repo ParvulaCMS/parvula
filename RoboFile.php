dabe550feeb1e1926ca5d8de697aca5600ce613b
BafS <fabacrans@gmail.com>
2017-05-22 15:00:17 +0200

    Add robo file to serve and test in one command
    Update travis script

---
+++ RoboFile.php
@@ -0,0 +1,22 @@
+<?php
+
+use \Robo\Tasks;
+
+/**
+ * Robo task runner.
+ */
+class RoboFile extends Tasks {
+
+	// Run all tests
+	public function test() {
+		// starts PHP server in background
+		$this->taskServer(8000)
+			->background()
+			->dir('public')
+			->run();
+
+		// Run unit tests
+		$this->taskCodecept()
+			->run();
+	}
+}
